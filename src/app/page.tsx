f19ed5d6526558c272145d720e8877c861abc656
Jose Angarita <amjose09@gmail.com>
2024-09-24 13:09:54 -0500

    eliminando archivos innecesarios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,12 @@
-import { TodoItem } from "@/components/TodoItem";
-import { Suspense } from "react";
 
 interface Props {
   searchParams: Record<string, string>;
 }
 
-const HomePage = ({ searchParams }: Props) => {
-  const id = parseInt(searchParams.id ?? "1", 10);
-
+const HomePage = ({}: Props) => {
   return (
     <div>
-      <Suspense fallback={<div>Loading...</div>}>
-        <TodoItem id={id} />
-      </Suspense>
+      Home Page
     </div>
   );
 };
