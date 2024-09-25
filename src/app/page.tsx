64229f6069141891290df320c9d974d6ff077fea
jorge <jorgecarvajal94@hotmail.com>
2024-09-25 10:14:38 -0500

    implementando disabled y hover en el button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/Button";
 
 interface Props {
   searchParams: Record<string, string>;
@@ -5,7 +6,26 @@ interface Props {
 
 const HomePage = ({}: Props) => {
   return (
-    <div></div>
+    <div className="flex gap-2 flex-col justify-center items-center">
+      <Button>button</Button>
+      <Button hoverActive="purpleLight">button</Button>
+      <Button className="hover:bg-purple-light">button</Button>
+      <Button className="bg-purple bg-opacity-25">button</Button>
+      <Button disabled={true}>button</Button>
+      <Button variant="secondary">button</Button>
+      <Button
+        variant="secondary"
+        className="hover:bg-purple-lightest border border-purple text-purple"
+      >
+        button
+      </Button>
+      <Button
+        variant="secondary"
+        className="bg-purple-lightest text-opacity-25 text-purple"
+      >
+        button
+      </Button>
+    </div>
   );
 };
 
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,15 +1,22 @@
 import { cn } from "@/util/cn";
 import { cva, type VariantProps } from "cva";
 
-const buttonStyles = cva("rounded-lg py-3 w-56", {
+const buttonStyles = cva("rounded-lg py-3 w-56 font-bold", {
   variants: {
     variant: {
       primary: "bg-purple text-white",
-      secondary: "",
+      secondary: "text-purple border-purple border",
+    },
+    hoverActive: {
+      purpleLight: "hover:bg-purple-light",
+    },
+    disabled: {
+      true: "bg-purple bg-opacity-25",
     },
   },
   defaultVariants: {
     variant: "primary",
+    disabled: false,
   },
 });
 
@@ -19,15 +26,21 @@ export interface ButtonProps
   extends React.HTMLAttributes<HTMLButtonElement>,
     ButtonStylesVariants {
   isLoading?: boolean;
+  disabled?: boolean;
 }
 
 export const Button: React.FC<ButtonProps> = ({
   variant,
   children,
   className,
+  hoverActive,
+  disabled,
   ...props
 }) => {
-  const buttonClassName = cn(buttonStyles({ variant }), className);
+  const buttonClassName = cn(
+    buttonStyles({ variant, disabled, hoverActive }),
+    className
+  );
 
   return (
     <button {...props} className={buttonClassName}>
