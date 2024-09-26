67f5a8c7b70dd3ecd943ef39f3edcf663eb7b9e4
Jose Angarita <jose.angarita@inchcape.com>
2024-09-26 13:54:19 -0500

    arreglando ultimos detalles en maquetación de login page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -7,37 +7,45 @@ import { Button } from "../../../components/ui/Button";
 
 const LoginPage = () => {
   return (
-    <div className="flex flex-col h-full px-3 items-start justify-start lg:items-center lg:justify-center">
-      <div className="flex items-center p-8 justify-center mb-4 gap-3 sm:mb-10">
+    <div className="flex flex-col h-full px-10 gap-10 items-center justify-center">
+      <div className="flex items-center gap-3 mr-auto md:mr-0">
         <Image src={Logo} alt="DevLinks" />
         <h2 className="text-4xl font-bold text-center">devlinks</h2>
       </div>
 
-      <div className="md:bg-white rounded-lg px-8 py-10 w-full max-w-md">
+      <div className="md:bg-white rounded-lg md:p-10 w-full md:max-w-md">
         <h1 className="text-3xl font-bold mb-2">Login</h1>
         <p className="text-gray mb-6 text-base leading-6">
           Add your details below to get back into the app
         </p>
 
-        <form className="flex flex-col space-y-4">
-          <div className="relative">
-            <label className="block text-gray-dark text-sm font-normal mb-1">
+        <form className="space-y-6">
+          <div className="relative flex flex-col gap-1">
+            <label
+              htmlFor="email"
+              className="block text-gray-dark text-sm font-normal"
+            >
               Email address
             </label>
-            <AiOutlineMail className="absolute left-3 top-[3.3rem] transform -translate-y-1/2 text-gray" />
+            <AiOutlineMail className="absolute left-3 top-[54px] -translate-y-1/2 text-gray" />
             <input
+              id="email"
               type="email"
               placeholder="e.g. alex@email.com"
               className="w-full pl-10 py-4 border border-borders rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
 
-          <div className="relative">
-            <label className="block text-gray-dark text-sm font-medium mb-1">
+          <div className="relative flex-col flex gap-1">
+            <label
+              htmlFor="password"
+              className="block text-gray-dark text-sm font-medium"
+            >
               Password
             </label>
-            <GiPadlock className="absolute left-3 top-[3.3rem] transform -translate-y-1/2 text-gray" />
+            <GiPadlock className="absolute left-3 top-[54px] -translate-y-1/2 text-gray" />
             <input
+              id="password"
               type="password"
               placeholder="Enter your password"
               className="w-full pl-10 py-4 border border-borders rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
