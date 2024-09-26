66caf3c3e722a864c4f0b88dc2668790e1b63acf
Jose Angarita <jose.angarita@inchcape.com>
2024-09-26 14:29:55 -0500

    creando nuevo componente text

---
+++ src/components/ui/Text.tsx
@@ -0,0 +1,34 @@
+import { cn } from "@/util/cn";
+import { cva, VariantProps } from "cva";
+import React from "react";
+
+const textStyles = cva("", {
+  variants: {
+    variant: {
+      "heading-m": "text-heading-m",
+      "heading-s": "text-heading-s",
+      "body-m": "text-body-m",
+      "body-s": "text-body-s",
+    },
+  },
+  defaultVariants: {
+    variant: "body-m",
+  },
+});
+
+type TextStylesVariants = VariantProps<typeof textStyles>;
+
+export interface TextProps extends TextStylesVariants {
+  as?: keyof JSX.IntrinsicElements;
+  [key: string]: unknown;
+}
+
+export const Text: React.FC<TextProps> = ({
+  as: Component = "p",
+  variant,
+  ...props
+}) => {
+  const classes = cn(textStyles({ variant }), props.className as string);
+
+  return <Component {...props} className={classes} />;
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,6 +22,24 @@ const config: Config = {
         borders: "#D9D9D9",
         red: "#FF3939",
       },
+      fontSize: {
+        'heading-m': ['2rem', {
+          lineHeight: '3rem',
+          fontWeight: '700'
+        }],
+        'heading-s': ['1rem', {
+          lineHeight: '1.5rem',
+          fontWeight: '600'
+        }],
+        'body-m': ['1rem', {
+          lineHeight: '1.5rem',
+          fontWeight: '400'
+        }],
+        'body-s': ['0.75rem', {
+          lineHeight: '1.125rem',
+          fontWeight: '400'
+        }],
+      },
     },
   },
   plugins: [],
