931e6a840486fe358d39f3ca63306c87f66b9a71
jorge <jorgecarvajal94@hotmail.com>
2024-09-26 13:30:58 -0500

    create responsive

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -7,13 +7,13 @@ import { Button } from "../../../components/ui/Button";
 
 const LoginPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-full">
-      <div className="flex items-center justify-center mb-8">
+    <div className="flex flex-col h-full px-3 items-start justify-start lg:items-center lg:justify-center">
+      <div className="flex items-center p-8 justify-center mb-4 gap-3 sm:mb-10">
         <Image src={Logo} alt="DevLinks" />
-        <h2 className="text-4xl font-bold text-center">DevLinks</h2>
+        <h2 className="text-4xl font-bold text-center">devlinks</h2>
       </div>
 
-      <div className="bg-white rounded-lg px-8 py-6 w-full max-w-md">
+      <div className="md:bg-white rounded-lg px-8 py-10 w-full max-w-md">
         <h1 className="text-3xl font-bold mb-2">Login</h1>
         <p className="text-gray mb-6 text-base leading-6">
           Add your details below to get back into the app
@@ -24,11 +24,11 @@ const LoginPage = () => {
             <label className="block text-gray-dark text-sm font-normal mb-1">
               Email address
             </label>
-            <AiOutlineMail className="absolute left-3 top-11 text-gray" />
+            <AiOutlineMail className="absolute left-3 top-[3.3rem] transform -translate-y-1/2 text-gray" />
             <input
               type="email"
               placeholder="e.g. alex@email.com"
-              className="w-full px-4 py-4 border border-borders rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="w-full pl-10 py-4 border border-borders rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
 
@@ -36,24 +36,24 @@ const LoginPage = () => {
             <label className="block text-gray-dark text-sm font-medium mb-1">
               Password
             </label>
-            <GiPadlock className="absolute left-3 top-11 text-gray" />
+            <GiPadlock className="absolute left-3 top-[3.3rem] transform -translate-y-1/2 text-gray" />
             <input
               type="password"
               placeholder="Enter your password"
-              className="w-full px-4 py-4 border border-borders rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="w-full pl-10 py-4 border border-borders rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
-
-          <span className="flex items-center justify-center text-base font-normal">
-            {"Don't have an account?"}
-            <Link href="/" className="text-purple ">
-              Create account
-            </Link>
-          </span>
-
-          <Button className="w-full font-semibold text-white py-2 rounded-lg">
-            Login
-          </Button>
+          <div className="flex flex-col gap-5">
+            <Button className="w-full font-semibold text-white py-2 rounded-lg">
+              Login
+            </Button>
+            <p className="flex md:flex-row md:gap-1 flex-col gap-px items-center justify-center text-base font-normal text-gray">
+              {"Don't have an account?"}
+              <Link href="/" className="text-purple">
+                Create account
+              </Link>
+            </p>
+          </div>
         </form>
       </div>
     </div>
