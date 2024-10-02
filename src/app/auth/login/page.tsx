5f69bb658ad47c641b52e53f7104ca10a690b880
jorge <jorgecarvajal94@hotmail.com>
2024-10-02 14:18:06 -0500

    implementacion react-hook-form

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Logo from "@/assets/logo.png";
 import Image from "next/image";
 import Link from "next/link";
@@ -5,8 +7,26 @@ import { AiOutlineMail } from "react-icons/ai";
 import { GiPadlock } from "react-icons/gi";
 import { Button } from "../../../components/ui/Button";
 import { Text } from "@/components/ui/Text";
+import { useForm } from "react-hook-form";
+
+interface LoginFormData {
+  email: string;
+  password: string;
+}
 
 const LoginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<LoginFormData>();
+
+  const onSubmit = (data: LoginFormData) => {
+    console.log(data);
+    reset();
+  };
+
   return (
     <div className="flex flex-col h-full px-10 gap-10 items-center justify-center">
       <div className="flex items-center gap-3 mr-auto md:mr-0">
@@ -24,7 +44,7 @@ const LoginPage = () => {
           Add your details below to get back into the app
         </Text>
 
-        <form className="space-y-6">
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
           <div className="relative flex flex-col gap-1">
             <Text variant="body-s" as="label" htmlFor="email">
               Email address
@@ -32,11 +52,22 @@ const LoginPage = () => {
 
             <AiOutlineMail className="absolute left-3 top-[53px] -translate-y-1/2 text-gray" />
             <input
+              {...register("email", {
+                pattern: {
+                  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                  message: "El formato del correo electrónico no es válido",
+                },
+                required: "Email es requerido",
+              })}
               id="email"
-              type="email"
               placeholder="e.g. alex@email.com"
               className="w-full pl-10 py-4 border border-borders rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
+            {errors.email && (
+              <Text variant="body-s" className="text-red">
+                {errors.email.message}
+              </Text>
+            )}
           </div>
 
           <div className="relative flex-col flex gap-1">
@@ -46,11 +77,23 @@ const LoginPage = () => {
 
             <GiPadlock className="absolute left-3 top-[54px] -translate-y-1/2 text-gray" />
             <input
+              {...register("password", {
+                required: "contraseña es requerida",
+                minLength: {
+                  value: 8,
+                  message: "la contraseña debe tener 8 caracteres",
+                },
+              })}
               id="password"
               type="password"
               placeholder="Enter your password"
               className="w-full pl-10 py-4 border border-borders rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
+            {errors.password && (
+              <Text variant="body-s" className="text-red">
+                {errors.password.message}
+              </Text>
+            )}
           </div>
           <div className="flex flex-col gap-5">
             <Button className="w-full font-semibold text-white py-2 rounded-lg">
