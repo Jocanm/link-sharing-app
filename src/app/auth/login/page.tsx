99a0f8823f0fddfb95c4f2140506c60a15002502
jorge <jorgecarvajal94@hotmail.com>
2024-10-14 12:26:58 -0500

    implementacion de provider context

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -2,7 +2,7 @@
 
 import Logo from "@/assets/logo.png";
 import { Button } from "@/components/ui/Button";
-import { Input } from "@/components/ui/Input";
+import InputForm from "@/components/ui/form/InputForm";
 import { Text } from "@/components/ui/Text";
 import { Routes } from "@/constants/routes";
 import {
@@ -11,25 +11,17 @@ import {
 } from "@/modules/auth/hooks/login/useLoginForm";
 import Image from "next/image";
 import Link from "next/link";
-import { FieldErrors } from "react-hook-form";
+import { FormProvider } from "react-hook-form";
 import { AiOutlineMail } from "react-icons/ai";
 import { GiPadlock } from "react-icons/gi";
 
 const LoginPage = () => {
-  const {
-    reset,
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useLoginForm();
+  const methods = useLoginForm();
+  const { reset, handleSubmit } = methods;
 
   const onSubmit = (data: LoginFormData) => {
     console.log(data);
     reset();
-  };
-
-  const onErrors = (errors: FieldErrors<LoginFormData>) => {
-    console.log(errors);
   };
 
   return (
@@ -49,45 +41,40 @@ const LoginPage = () => {
           Add your details below to get back into the app
         </Text>
 
-        <form className="space-y-6" onSubmit={handleSubmit(onSubmit, onErrors)}>
-          <div className="relative flex flex-col gap-1">
-            <Input
+        <FormProvider {...methods}>
+          <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+            <InputForm
+              name="email"
               label="Email address"
               placeholder="e.g. alex@email.com"
               Icon={<AiOutlineMail />}
-              errorValidation={errors.email?.message}
-              {...register("email")}
             />
-          </div>
-
-          <div className="relative flex-col flex gap-1">
-            <Input
+            <InputForm
+              name="password"
               label="Password"
               type="password"
               placeholder="Enter your password"
               Icon={<GiPadlock />}
-              errorValidation={errors.password?.message}
-              {...register("password")}
             />
-          </div>
-          <div className="flex flex-col gap-5">
-            <Button
-              type="submit"
-              className="w-full font-semibold text-white py-2 rounded-lg"
-            >
-              Login
-            </Button>
-            <Text
-              variant="body-m"
-              className="flex md:flex-row md:gap-1 flex-col gap-px items-center justify-center text-gray"
-            >
-              {"Don't have an account?"}
-              <Link href={Routes.REGISTER} className="text-purple">
-                <Text variant="body-m">Create account</Text>
-              </Link>
-            </Text>
-          </div>
-        </form>
+            <div className="flex flex-col gap-5">
+              <Button
+                type="submit"
+                className="w-full font-semibold text-white py-2 rounded-lg"
+              >
+                Login
+              </Button>
+              <Text
+                variant="body-m"
+                className="flex md:flex-row md:gap-1 flex-col gap-px items-center justify-center text-gray"
+              >
+                {"Don't have an account?"}
+                <Link href={Routes.REGISTER} className="text-purple">
+                  <Text variant="body-m">Create account</Text>
+                </Link>
+              </Text>
+            </div>
+          </form>
+        </FormProvider>
       </div>
     </div>
   );
