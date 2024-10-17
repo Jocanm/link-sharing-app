0d607a1996724fcadf8d83e7e82e0a60f8381dc3
jorge <jorgecarvajal94@hotmail.com>
2024-10-16 19:13:33 -0500

    creacion navbar

---
+++ src/app/configuration/links/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Links = () => {
+  return (
+    <div className="flex items-center justify-center text-black">
+      Hello Links
+    </div>
+  );
+};
+
+export default Links;
