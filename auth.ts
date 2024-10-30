8f5c61f004a7cad05dfe8038cc780bc2699b1269
jorge <jorgecarvajal94@hotmail.com>
2024-10-30 10:23:26 -0500

    removiendo adapter

--- auth.ts
+++ auth.ts
@@ -1,11 +1,7 @@
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
 
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { prisma } from "@/lib/db";
-
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
   ...authConfig,
 });
